bcacb253edae6245d7b9abe34c2ec6e0578597ba
Bart Van Bos <bartvanbos@gmail.com>
2020-11-26 21:50:42 +0100

    added calico config

--- common/k3s/cluster2/install-master.sh
+++ common/k3s/cluster2/install-master.sh
@@ -24,6 +24,3 @@ echo 'export KUBECONFIG=/home/ubuntu/.kube/config' >>~/.bashrc
 echo 'source <(kubectl completion bash)' >>~/.bashrc
 echo 'alias k=kubectl' >>~/.bashrc
 echo 'complete -F __start_kubectl k' >>~/.bashrc
-
-kubectl create -f tigera-operator.yml
-kubectl create -f custom-resources.yml
