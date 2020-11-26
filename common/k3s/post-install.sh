a1437fc1196d6d30fb29bd6090912b1339770151
Bart Van Bos <bartvanbos@gmail.com>
2020-11-26 21:54:02 +0100

    added calico config

--- common/k3s/post-install.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-kubectl create -f https://docs.projectcalico.org/manifests/tigera-operator.yaml
-kubectl create -f https://docs.projectcalico.org/manifests/custom-resources.yaml
--- common/k3s/uninstall.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-# Uninstall K3S
-k3s-agent-uninstall.sh
-k3s-uninstall.sh
