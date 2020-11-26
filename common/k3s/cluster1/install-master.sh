58c4f11bd1cc9b123ac7ffd62f76c133d3491085
Bart Van Bos <bartvanbos@gmail.com>
2020-11-26 23:36:04 +0100

    fix multinetworking

--- common/k3s/cluster1/install-master.sh
+++ common/k3s/cluster1/install-master.sh
@@ -4,7 +4,6 @@ K3S_VERSION=v1.18.12+k3s1
 MASTER_IP=10.1.20.5
 INSTALL_K3S_EXEC="--flannel-backend=none \
                   --flannel-iface=ens6 \
-                  --bind-address=${MASTER_IP} \
                   --advertise-address=${MASTER_IP} \
                   --cluster-cidr=192.168.1.0/24 \
                   --cluster-domain=cluster1.local \
