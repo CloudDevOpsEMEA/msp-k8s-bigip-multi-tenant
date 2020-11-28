657de7988ec133da0b95c1857f1d0b48af16b78b
Bart Van Bos <bartvanbos@gmail.com>
2020-11-28 19:49:35 +0100

    refactor cert harbor mess

--- operations/harbor/cert.sh
+++ operations/harbor/cert.sh
@@ -3,9 +3,11 @@
 CA_CERT_DIR=../../certificates/ca
 HARBOR_CERT_DIR=../../certificates/harbor
 
+
 echo "Provide the Certificates to Harbor and Docker"
-sudo cp ${HARBOR_CERT_DIR}/harbor.crt /home/ubuntu/harbor/certs
-sudo cp ${HARBOR_CERT_DIR}/harbor.key /home/ubuntu/harbor/certs
+mkdir -p /home/ubuntu/harbor/certs
+cp ${HARBOR_CERT_DIR}/harbor.crt /home/ubuntu/harbor/certs
+cp ${HARBOR_CERT_DIR}/harbor.key /home/ubuntu/harbor/certs
 
 sudo mkdir -p /var/snap/docker/certs.d/harbor:8443/
 sudo cp ${HARBOR_CERT_DIR}/harbor.cert /var/snap/docker/certs.d/harbor:8443/
