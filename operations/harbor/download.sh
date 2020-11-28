30b1d9f7a99ee411adaa4f9af0532cebe48da39d
Bart Van Bos <bartvanbos@gmail.com>
2020-11-28 19:21:37 +0100

    refactor cert harbor mess

--- operations/harbor/download.sh
+++ operations/harbor/download.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 
 HARBOR_VERSION=v2.0.4
+INSTALL_DIR=/home/ubuntu/harbor
 
 echo "Download harbor docker registry tarball"
-sudo mkdir /opt/harbor
-sudo chown -R ubuntu:ubuntu /opt/harbor
-cd  /opt/harbor
 wget https://github.com/goharbor/harbor/releases/download/${HARBOR_VERSION}/harbor-online-installer-${HARBOR_VERSION}.tgz
+mv harbor-online-installer-${HARBOR_VERSION}.tgz /tmp
+cd /tmp
 tar xvf harbor-online-installer-${HARBOR_VERSION}.tgz
+cd harbor
