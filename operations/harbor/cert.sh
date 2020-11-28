6e977ab52d2bbc3301b89d52e3831e1cd899d00d
Bart Van Bos <bartvanbos@gmail.com>
2020-11-28 20:44:58 +0100

    refactor cert harbor mess

--- operations/harbor/cert.sh
+++ operations/harbor/cert.sh
@@ -13,4 +13,19 @@ sudo mkdir -p /var/snap/docker/certs.d/harbor:8443/
 sudo cp ${HARBOR_CERT_DIR}/harbor.cert /var/snap/docker/certs.d/harbor:8443/
 sudo cp ${HARBOR_CERT_DIR}/harbor.key /var/snap/docker/certs.d/harbor:8443/
 sudo cp ${CA_CERT_DIR}/ca.crt /var/snap/docker/certs.d/harbor:8443/
+sudo snap set system store-certs.cert1="$(cat ${CA_CERT_DIR}/ca.crt)"
 sudo snap restart docker
+
+sudo mkdir -p /var/snap/docker/common/etc/certs.d/harbor:8443
+sudo cp ${HARBOR_CERT_DIR}/harbor.cert /var/snap/docker/common/etc/certs.d/harbor:8443
+sudo cp ${HARBOR_CERT_DIR}/harbor.key /var/snap/docker/common/etc/certs.d/harbor:8443
+sudo cp ${CA_CERT_DIR}/ca.crt /var/snap/docker/common/etc/certs.d/harbor:8443
+
+
+sudo cp ${CA_CERT_DIR}/ca.crt /usr/local/share/ca-certificates/harbor.udfdemo.org:8443.crt
+sudo cp ${CA_CERT_DIR}/ca.crt /usr/local/share/ca-certificates/udfdemo.org:8443.crt
+sudo cp ${CA_CERT_DIR}/ca.crt /usr/local/share/ca-certificates/harbor:8443.crt
+sudo cp ${CA_CERT_DIR}/ca.crt /usr/local/share/ca-certificates/harbor.udfdemo.org.crt
+sudo cp ${CA_CERT_DIR}/ca.crt /usr/local/share/ca-certificates/udfdemo.org.crt
+sudo cp ${CA_CERT_DIR}/ca.crt /usr/local/share/ca-certificates/harbor.crt
+sudo update-ca-certificates
